74beb3633a0d59fbeac10627a7929e8ca9c2bbc8
AI-Amponsah <Yawamp27@gmail.com>
2023-01-02 05:02:14 +0000

    print

---
+++ Intoduction/printing-in_python.py
@@ -0,0 +1,20 @@
+#In python 2, parenthesis are not needed around the phrase one wants to print
+
+name = "isaAc"
+#print a message to the name stored in the variable
+
+print("Hello " + name.title() + "," "" + " "+ "Do you want to learn Python?")
+
+#Store a person’s name in a variable, and then print that person’s name in lowercase, 
+#uppercase, and titlecase
+
+full_name = "isAAc yAw aMpONsah"
+print(full_name.lower())
+print(full_name.upper())
+print(full_name.title())
+
+famous_name = "Richard P. Feynman"
+message = '"What one fool can understand, another can."'
+
+print(famous_name + " " + "Once said," + '"What one fool can understand, another can."\n')
+print(famous_name + " " "Once said," + message)

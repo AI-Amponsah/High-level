d6692af07621531ecc3f61ad47a945993db82352
AI-Amponsah <Yawamp27@gmail.com>
2023-01-02 01:45:59 +0000

    strings

--- Intoduction/strings.py
+++ Intoduction/strings.py
@@ -1,4 +1,20 @@
+#methods in python
 name = "isAaC aMponsah"
 print(name.title())
 print(name.upper())
 print(name.lower())
+
+#combining strings in python
+#string concatenation
+
+first_name = "Isaac"
+last_name = "Amponsah"
+
+full_name = first_name + " " + last_name
+print("Full name is " + full_name)
+
+#uisng white spaces, tabs and newlines
+
+print("Python")
+print("\tPython")
+print("Apples\nOranges\nMangoes")

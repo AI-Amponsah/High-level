577cf9a8f91703fe0f187ff43b49da96c0f73f64
AI-Amponsah <Yawamp27@gmail.com>
2023-01-03 01:25:26 +0000

    lists

--- Intoduction/lists.py
+++ Intoduction/lists.py
@@ -8,3 +8,19 @@ print(market_stuff)
 print(market_stuff[0].title())
 print(market_stuff[1].title())
 print(market_stuff[2].title())
+
+#python has a special syntax for accessing last element in a list
+#The use of -1 is use to access the last element in a list
+print(market_stuff[-1])
+
+#Exercise
+#3-1 Names: : Store the names of a few of your friends in a list called names.
+#solution
+
+names = ['Ama', 'Yaw', 'Esther', 'Amponsah']
+print(names[-2])
+
+#3-2 Greetings: 
+message = "Hello" + " " + names[1].title() + "," + "" + "Congrats!"
+print(message)
+

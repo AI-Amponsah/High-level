e1117ec8ffe08aa781ed1b17a202ba5b5e3f2278
AI-Amponsah <Yawamp27@gmail.com>
2023-01-03 01:13:07 +0000

    lists

---
+++ Intoduction/lists.py
@@ -0,0 +1,10 @@
+#lists in python are represented by []
+#We access elements in a list using their indices
+#Elements in a list are separated by comma
+
+market_stuff = ['Fish', 'bags', 'tomatoes']
+print(market_stuff)
+
+print(market_stuff[0].title())
+print(market_stuff[1].title())
+print(market_stuff[2].title())
---
+++ Intoduction/numbers.py
@@ -0,0 +1,13 @@
+#add, subtract, multiply and divide integers
+print(2 + 5)
+print(8 - 7) 
+print(7 * 0.1)
+print(3/2)
+
+##exponents are represented by two **
+
+print(2**2)
+
+age = 23
+#the str() function converts a nn-string value to a string
+print("I am" + " " + str(age) + " " +"years")h

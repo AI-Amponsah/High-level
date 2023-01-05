3c414a660e459206075e2fff91b24a030ace27cd
AI-Amponsah <Yawamp27@gmail.com>
2023-01-05 01:41:23 +0000

    py

--- Intoduction/py.py
+++ Intoduction/py.py
@@ -1,6 +1,20 @@
 #Converting miles to kg
 #Ask user for miles input
-
 miles = int(input("Enter distance:" ))
 kg = miles *  1.60934
 print("{} miles equals {} kilometers".format(miles, kg))
+
+#Program to determine the grade of a person based on their age
+
+#Ask for the persons age
+
+age = eval(input("Enter your age: " ))
+
+if age == 5:
+    print("Kindergarten")
+elif (age>= 6) and (age<= 17):
+    print("From grades 1 to 12.")
+elif age > 17:
+    print("College")
+else:
+    print("Input the correct age range")

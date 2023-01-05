9f9147f3f7b5ce9dc787856bb8906720f7b515fb
AI-Amponsah <Yawamp27@gmail.com>
2023-01-05 01:15:34 +0000

    py

--- Intoduction/formar_strings.py
+++ Intoduction/formar_strings.py
@@ -1,4 +1,4 @@
 #the str.format allows for variable substitution and string concatenation
 
-message = "Hello {} {} {}," + " " + "How are you doing?"
+message = "Hello {2} {1} {0}," + " " + "How are you doing?"
 print(message.format("Isaac", "Yaw", "Amponsah"))
---
+++ Intoduction/py.py
@@ -0,0 +1,6 @@
+#Converting miles to kg
+#Ask user for miles input
+
+miles = int(input("Enter distance:" ))
+kg = miles *  1.60934
+print("{} miles equals {} kilometers".format(miles, kg))

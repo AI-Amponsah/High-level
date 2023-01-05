f84eac5f38b388355cd935a15f423a6dc1bf0f51
AI-Amponsah <Yawamp27@gmail.com>
2023-01-05 02:12:54 +0000

    loop

--- Loops/for_loop.py
+++ Loops/for_loop.py
@@ -4,5 +4,13 @@ for i in [1,5,8,7,3]:
     print("i = ", i)
 
 #using range
-for i in range(20):
+for i in range(5, 20, 5):
     print("i = ", i)
+
+#printing odds from 0 to 20
+
+for i in range(21):
+    if (i % 2) != 0:
+        print("{} is odd".format(i))
+    else:
+         print("{} is even".format(i))

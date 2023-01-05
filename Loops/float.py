59e2289fb8ec334f04419b824ae3ef468b1651d6
AI-Amponsah <Yawamp27@gmail.com>
2023-01-05 02:34:23 +0000

    float

--- Loops/float.py
+++ Loops/float.py
@@ -2,3 +2,14 @@
 
 number = float(input("Enter a float: "))
 print("The value in 2dp is {:.2f}".format(number))
+
+#simple compound interest code
+
+principal = input("Enter the Principal and Rate: ")
+rate = input("Enter the  Rate: ")
+principal = float(principal)
+rate = float(rate) * .01
+
+for i in range(10):
+    principal = principal + (principal * rate)
+print("The pricipal after 10 years is {:.2f}".format(principal))

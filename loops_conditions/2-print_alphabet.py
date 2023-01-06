e2a3f45fbefdb4355841f12f334c09d0370689ac
AI-Amponsah <Yawamp27@gmail.com>
2023-01-06 01:43:32 +0000

    alpha

--- loops_conditions/2-print_alphabet.py
+++ loops_conditions/2-print_alphabet.py
@@ -2,4 +2,5 @@ for i in range(ord('a'), ord('z') + 1):
     print("{:s}".format(chr(i)))
 
 for i in range(97, 123):
-    print("{:c}".format(i))
+    if i != 101 and i != 113:
+        print("{:c}".format(i))

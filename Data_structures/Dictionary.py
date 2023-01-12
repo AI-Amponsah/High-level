39e2eca78bfaa83fd1a5e2dee4ac3f687d592c55
AI-Amponsah <Yawamp27@gmail.com>
2023-01-12 00:47:59 +0000

    notes

--- Data_structures/Dictionary.py
+++ Data_structures/Dictionary.py
@@ -25,10 +25,19 @@ print(my_dict)
 '''
  # ACCESSING ITEMS IN DICTIONARIES
  # One way of accessing items in a dictionary is to use the get() method
- # example:  x = my_dict.get("Name")
- #             print(x)
+ '''example:  x = my_dict.get("Name")'''
+             '''print(x)'''
  # To get the keys in a dictionary, we use the key() method
- # i.e x = my_dict.keys() , this returns the keys in a list[] inside a tuple()
+ '''i.e x = my_dict.keys() , this returns the keys in a list[] inside a tuple()'''
  
  # Likewise, the values of the dictionary can also be gotten using the values() method
  # i.e x = my_dict.values(), this returns all the values of the dictionary in a list[] inside a tuple()
+ # The items() method returns each item in the dictionary as tuples in a list
+ ''' i.e  my_dict.items()'''
+ 
+ #  CHECKING IF KEY EXISTS IN A DICTIONARY
+ # To check if a key exists in a dictionary we use th in keyword
+ ''' i.e if "Name" in my_dict
+      print("Name")
+ '''
+ 

a084950ba0d42e75b29d84b77d2323aed7401713
AI-Amponsah <Yawamp27@gmail.com>
2023-01-12 00:39:08 +0000

    notes

---
+++ Data_structures/Dictionary.py
@@ -0,0 +1,34 @@
+#Dictionaries are used to store data values in key pairs
+#Dictionaries are represented with curly braces i.e {}
+
+#  PROPERTIES OF DICTIONARIES
+    #Ordered
+    #changeable
+    #Do not allow duplicates
+'''
+eg: of a dictionary
+
+my_dict = {
+    "Name": "Isaac"
+    "Age": 27
+    "Occupation": "student"    
+}
+print(my_dict)
+'''
+#to tell the length of a dictionary, we use the len() fumction
+# i.e len(my_dict)
+# Aside curly braces {}, the dict() constructor is also used to make a dictionary
+# i.e 
+'''
+my_dict = dict("Name" = "Isaac", "Age" = 27, "Occupation" = "student")
+print(my_dict)
+'''
+ # ACCESSING ITEMS IN DICTIONARIES
+ # One way of accessing items in a dictionary is to use the get() method
+ # example:  x = my_dict.get("Name")
+ #             print(x)
+ # To get the keys in a dictionary, we use the key() method
+ # i.e x = my_dict.keys() , this returns the keys in a list[] inside a tuple()
+ 
+ # Likewise, the values of the dictionary can also be gotten using the values() method
+ # i.e x = my_dict.values(), this returns all the values of the dictionary in a list[] inside a tuple()

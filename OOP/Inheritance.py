1e55b49fe906a85704cd7fa077dd54e428f88e3f
AI-Amponsah <Yawamp27@gmail.com>
2023-02-08 02:04:39 +0000

    inheritance

---
+++ OOP/Inheritance.py
@@ -0,0 +1,6 @@
+class Mode_of_Transport:
+    def __init__(self):
+        self.vehicle = "Benz"
+        self.weight = 890
+transport = Mode_of_Transport()
+print(transport.vehicle, transport.weight)
